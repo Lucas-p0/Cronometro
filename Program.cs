da99aec418a4c65b4d18a0abaa755b64026effcb
Lucas pinheiro <lucaslogos@gmail.com>
2023-02-24 08:46:49 -0300

    update

---
+++ Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Cronometro
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Start();
+        }
+
+        static void Start()
+        {
+            int tempo = 10;
+            int tempoAtual = 0;
+
+            while (tempoAtual != tempo)
+            {
+                tempoAtual++;
+            }
+        }
+    }
+}
