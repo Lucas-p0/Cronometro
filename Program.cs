db89ad0ac787e4a9eb0989ba9c396838d74de063
Lucas pinheiro <lucaslogos@gmail.com>
2023-02-27 13:22:08 -0300

    update

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Threading;
 
 namespace Cronometro
 {
@@ -6,6 +7,7 @@ namespace Cronometro
     {
         static void Main(string[] args)
         {
+
             Start();
         }
 
@@ -16,7 +18,10 @@ namespace Cronometro
 
             while (tempoAtual != tempo)
             {
+                Console.Clear();
                 tempoAtual++;
+                Console.WriteLine(tempoAtual);
+                Thread.Sleep(1000);
             }
         }
     }
