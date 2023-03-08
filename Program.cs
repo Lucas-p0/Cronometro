60c6d7779240c59cf9988a1a5c0ff088864559c2
Lucas pinheiro <lucaslogos@gmail.com>
2023-03-08 12:58:37 -0300

    update

--- Program.cs
+++ Program.cs
@@ -14,14 +14,17 @@ namespace Cronometro
         static void Menu()
         {
             Console.Clear();
-            Console.WriteLine("S = Segundo => 10s = 10 segundos");
-            Console.WriteLine("M = Minuto => 10s = 10 segundos");
+            Console.WriteLine("S = Segundo => 10s = 10 Segundos");
+            Console.WriteLine("M = Minuto => 10m = 10 Minutos");
             Console.WriteLine("0 = Sair");
             Console.WriteLine("Quanto tempo deseja contar?");
 
             string data = Console.ReadLine().ToLower();
             char tipo = char.Parse(data.Substring(data.Length - 1, 1));
-            Console.WriteLine(data);
+            int tempo = int.Parse(data.Substring(0, data.Length - 1));
+
+            Console.WriteLine(tipo);
+            Console.WriteLine(tempo);
 
         }
 
